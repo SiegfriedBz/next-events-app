11dd47126be89a971df9f1f5eb54e56cc12209a6
Siegfried <siegfried.bozza@renuo.ch>
2023-02-28 21:49:27 +0100

    first commit

---
+++ pages/events/[events_by_city]/index.js
@@ -0,0 +1,15 @@
+const EventsByCityPage = () => {
+
+    return (
+        <div>
+            <h1>Events in London</h1>
+            <div>
+                <a href=""><img src="" alt=""/><h2>Event 1</h2></a>
+                <a href=""><img src="" alt=""/><h2>Event 2</h2></a>
+                <a href=""><img src="" alt=""/><h2>Event 3</h2></a>
+            </div>
+        </div>
+    )
+}
+
+export default EventsByCityPage
